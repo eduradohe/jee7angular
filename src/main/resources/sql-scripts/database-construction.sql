83831b4171e0975d5e801a32eb6a891621e32c28
Eduardo Turella <eduardohe@gmail.com>
2017-09-11 14:31:40 -0300

    Adding SQL file for creating basic database, tables and constraints

---
+++ src/main/resources/sql-scripts/database-construction.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE mobicare
+  WITH ENCODING='UTF8'
+       CONNECTION LIMIT=-1;
+
+CREATE TABLE public.department
+(
+    id integer NOT NULL,
+    name character varying(30) NOT NULL,
+    budget numeric(7, 2) NOT NULL DEFAULT 0.00,
+    CONSTRAINT pk_department_id PRIMARY KEY (id),
+    CONSTRAINT unique_department_name UNIQUE (name)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.department
+    OWNER to postgres;
+    
+CREATE TABLE public.employee
+(
+    id integer NOT NULL,
+    name character varying(30) NOT NULL,
+    department_id integer NOT NULL,
+    CONSTRAINT pk_employee_id PRIMARY KEY (id),
+    CONSTRAINT fk_employee_department_id FOREIGN KEY (department_id)
+        REFERENCES public.department (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.employee
+    OWNER to postgres;

95d2263953bb675cb6aa6d89f2b2856e2b01e1a5
Eduardo Turella <eduardohe@gmail.com>
2017-09-12 10:36:47 -0300

    Added pagination to Employees listing page

---
+++ src/main/java/br/com/mobicare/view/services/EmployeeViewService.java
@@ -0,0 +1,57 @@
+package br.com.mobicare.view.services;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.json.JsonArray;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+
+import br.com.mobicare.model.entities.Employee;
+import br.com.mobicare.view.util.PaginatedListWrapper;
+import br.com.mobicare.view.util.ViewUtils;
+
+@Stateless
+@ApplicationPath("/view")
+@Path("employeesView")
+public class EmployeeViewService extends Application {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public PaginatedListWrapper<Employee> listEmployees(@DefaultValue("1")
+													    @QueryParam("page")
+													    Integer page,
+													    @DefaultValue("id")
+													    @QueryParam("sortFields")
+													    String sortFields,
+													    @DefaultValue("asc")
+													    @QueryParam("sortDirections")
+													    String sortDirections) {
+		
+		final PaginatedListWrapper<Employee> wrapper = new PaginatedListWrapper<>();
+		wrapper.setCurrentPage(page);
+		wrapper.setSortFields(sortFields);
+		wrapper.setSortDirections(sortDirections);
+		wrapper.setPageSize(5);
+		
+		final Client client = ClientBuilder.newClient();
+		final WebTarget target = client.target("http://localhost:8080/java-pleno-eduardo-turella/services/employees");
+		final JsonArray response = target.request(MediaType.APPLICATION_JSON).get(JsonArray.class);
+		
+		final List<Employee> employees = ViewUtils.parseJsonToList(response);
+		
+		wrapper.setList(employees);
+		wrapper.setTotalResults(employees.size());
+		
+		return wrapper;
+	}
+}
--- src/main/webapp/script/employee.js
+++ src/main/webapp/script/employee.js
@@ -1,35 +1,53 @@
 var app = angular.module('employees', ['ngGrid', 'ui.bootstrap']);
 
-app.controller('employeesList', function ($scope, $http) {
-	
-	$scope.refreshGrid = function () {
-		$http({
-			url: 'services/employees',
-			method: 'GET'
-		}).success(function (data) {
-			$scope.employees = data;
-		});
-	};
-	
+// Create a controller with name employeesList to bind to the html page.
+app.controller('employeesList', function ($scope, employeeService) {
+    // Refresh the grid, calling the appropriate service method.
+    $scope.refreshGrid = function (page) {
+        employeeService.getAll(page, $scope.sortInfo.fields[0], $scope.sortInfo.directions[0]).success(function (data) {
+            $scope.employees = data;
+        });
+    };
+
+    // Do something when the grid is sorted.
+    // The grid throws the ngGridEventSorted that gets picked up here and assigns the sortInfo to the scope.
+    // This will allow to watch the sortInfo in the scope for changed and refresh the grid.
     $scope.$on('ngGridEventSorted', function (event, sortInfo) {
         $scope.sortInfo = sortInfo;
     });
-	
-	$scope.$watch('sortInfo', function() {
-		$scope.refreshGrid();
-	}, true);
-	
-	$scope.sortInfo = {fields: ['id'], directions: ['asc']};
-	$scope.employees = null;
+
+    // Watch the sortInfo variable. If changes are detected than we need to refresh the grid.
+    // This also works for the first page access, since we assign the initial sorting in the initialize section.
+    $scope.$watch('sortInfo.fields[0]', function () {
+        $scope.refreshGrid($scope.employees.currentPage);
+    }, true);
+
+    // Initialize required information: sorting, the first page to show and the grid options.
+    $scope.employees = {currentPage: 1};
+    $scope.sortInfo = {fields: ['id'], directions: ['asc']};
     $scope.gridOptions = {
-    		data: 'employees',
-            useExternalSorting: true,
-            sortInfo: $scope.sortInfo,
-            columnDefs: [
-            	{field: 'id', displayName: 'ID'},
-            	{field: 'name', displayName: 'Name'},
-            	{field: 'income', displayName: 'Income'},
-            	{field: 'department.name', displayName: 'Department'}
-            ]
+        data: 'employees.list',
+        useExternalSorting: true,
+        sortInfo: $scope.sortInfo,
+        columnDefs: [
+        	{field: 'id', displayName: 'ID'},
+        	{field: 'name', displayName: 'Name'},
+        	{field: 'income', displayName: 'Income'},
+        	{field: 'department.name', displayName: 'Department'}
+        ]
     };
+});
+
+// Service that provides employees operations
+app.service('employeeService', function ($http) {
+    // Makes the REST request to get the data to populate the grid.
+    this.getAll = function (page, sortFields, sortDirections) {
+        return $http.get('view/employeesView', {
+            params: {
+                page: page,
+                sortFields: sortFields,
+                sortDirections: sortDirections
+            }
+        });
+    }
 });
